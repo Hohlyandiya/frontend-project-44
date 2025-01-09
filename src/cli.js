da8729056a55f8e984009c0a986406bc9c62a475
Hohlyandiya <teroryk@gmail.com>
2025-01-09 18:18:38 +0300

    test

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,4 @@
-import readlineSync from 'readline-sync';
+import readlineSync from '../readline-sync/lib/readline-sync.js';
 
 export const getNameUser = () => {
     console.log('Welcome to the Brain Games!');
