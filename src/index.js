cb5bb18b630a31107765bc02fda23c8d59ab2a15
Hohlyandiya <teroryk@gmail.com>
2025-01-09 18:20:10 +0300

    return

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import readlineSync from '../readline-sync/lib/readline-sync.js';
+import readlineSync from '../readline-sync';
 
 export const getResultBrainGame = (nameUser, answer, expression) => {
     console.log(`Question: ${expression}`);
